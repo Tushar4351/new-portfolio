69c114fdb77faf684d8cf8f7ecdbaf5de8aec4c9
Tushar Bhowal <tusharbhowal3211@gmail.com>
2024-01-15 23:55:40 +0530

    third commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+		  purple: "#7e5bef",
+		  black1: "#343434",
+		  principal: "#ffa97a",
+		  Blanc: "#fcfafa",
+		  gray1: "#484848",
+		  pink1: "#DE3163",
+		  green1: "#aac402",
+    },
+   
+    fontFamily: {
+      argenExtra: ["ArgentumSans-ExtraBold"],
+      argenBold: ["ArgentumSans-Bold"],
+      argenMed:["ArgentumSans-Medium"]
+    },
+    extend: {
+      screens: {
+        'xll': '1380px',
+      },
+    },
+  },
+  plugins: [],
+};
